f6ccd8c1db6d94a90e941a79a352b04dd5fef155
Taesiri <taesiri@live.com>
2015-03-04 21:45:05 +0330

    POST Requests

--- server.go
+++ server.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
@@ -15,7 +14,7 @@ const serverIP = "http://127.0.0.1:8000/"
 const serverPort = ":8000"
 const encryptedLink = "http://127.0.0.1:8000/enc/"
 
-var linksRegexp = regexp.MustCompile("\"(http|https)://([a-zA-Z0-9+&=#.(){};:,.<>_+?|\\\\/\\-]*)\"")
+var linksRegexp = regexp.MustCompile("\"(http|https)://([a-zA-Z0-9+&%=#.(){};:,.<>_+?|\\\\/\\-]*)\"")
 
 func check(e error) {
 	if e != nil {
@@ -31,8 +30,9 @@ func main() {
 	http.HandleFunc("/enc/", func(w http.ResponseWriter, r *http.Request) {
 		theUrl := strings.SplitN(r.URL.Path, "/", 3)[2]
 
-		log.Printf("ENC")
+		r.ParseForm()
 		log.Printf(theUrl)
+		log.Println(r.PostForm)
 
 		sDec, _ := base64.StdEncoding.DecodeString(theUrl)
 		sDec, _ = base64.StdEncoding.DecodeString(string(sDec[:]))
@@ -44,19 +44,20 @@ func main() {
 			Jar: cookieJar,
 		}
 
-		resp, err := client.Get(address)
+		var resp *http.Response
+		if len(r.PostForm) == 0 {
+			resp, _ = client.Get(address)
+		} else {
+			resp, _ = client.PostForm(address, r.PostForm)
+		}
+
 		body, err := ioutil.ReadAll(resp.Body)
 		check(err)
 
 		s := string(body[:])
 
 		output := linksRegexp.FindAllString(s, -1)
-		log.Println(output)
-		log.Println(strconv.Itoa(len(output)))
-
 		for _, link := range output {
-			log.Println(link)
-
 			secureLink := base64.StdEncoding.EncodeToString([]byte(link[1 : len(link)-1]))
 			secureLink = base64.StdEncoding.EncodeToString([]byte(secureLink))
 
